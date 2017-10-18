5bad1b3ecefd8a4ee2f5699f8542c76bc8305e50
Strohmeier Anton 1CP2 <anton.strohmeier@rohde-schwarz.com>
2017-10-18 12:21:31 +0200

    step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class ContactsAppComponent {
   title = 'Angular Master Class setup works!';
+  contact: Contact = {
+ id: 1,
+        name: 'Pascal Precht',
+        email: 'pascal@thoughtram.io',
+        phone: '+49 000 222',
+        birthday: '1991-03-31',
+        website: 'thoughtram.io',
+        image: '/assets/images/1.jpg',
+        address: {
+          street: 'thoughtram road 1',
+          zip: '65222',
+          city: 'Hanover',
+          country: 'Germany'
+        }
+  }
+
 }
